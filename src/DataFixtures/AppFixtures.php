875304184b2887bc02b9598b403e001895f8347b
AlixRomain <romain.alix89@gmail.com>
2021-05-10 10:53:52 +0200

    Setting environement with jwt and add fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Tools;
+use App\Entity\User;
+use App\Repository\ToolsRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $repoTools;
+    private $faker;
+    private $encoder;
+    public function __construct( UserPasswordEncoderInterface $encoder, ToolsRepository $repoTools)
+    {
+        $this->repoTools = $repoTools;
+        $this->faker = Factory::create("fr_FR");
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        //FOR LOAD METHOD WITH DROP TABLE "symfony console doctrine:fixtures:load"
+        //FOR LOAD METHOD WITHOUT DROP TABLE "symfony console doctrine:fixtures:load --append"
+        $contenu_fichier_json = file_get_contents(__DIR__.'/datas.json');
+        $datas = json_decode($contenu_fichier_json, true);
+
+        foreach($datas['tools'] as $tools ){
+            $user = new User();
+            $user->setUsername($this->faker->userName)
+                ->setEmail($this->faker->email)
+                ->setRoles(User::ROLE_USER)
+                ->setPassword($this->encoder->encodePassword($user, "Hub3E2021"));
+            $manager->persist($user);
+
+            $newTools = new Tools();
+            $newTools->setName($tools["name"])
+                ->setDescription($tools["description"])
+                ->setRelation($user);
+            $manager->persist($newTools);
+        }
+        $user = new User();
+        $user->setUsername("admin")
+            ->setEmail("admin@admin.fr")
+            ->setRoles(User::ROLE_ADMIN)
+            ->setPassword($this->encoder->encodePassword($user, "Hub3E2021"));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Tools.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ToolsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ToolsRepository::class)
+ */
+class Tools
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="tools")
+     */
+    private $relation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getRelation(): ?User
+    {
+        return $this->relation;
+    }
+
+    public function setRelation(?User $relation): self
+    {
+        $this->relation = $relation;
+
+        return $this;
+    }
+}
